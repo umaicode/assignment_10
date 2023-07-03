f971fc10006c5c1076269b6c7ea9c11803185cb2
umaicode <park.seungchan99@gmail.com>
2023-07-03 20:05:28 +0900

    revised

--- practice.cpp
+++ practice.cpp
@@ -1,5 +1,7 @@
 /*
 * 객체들의 Set을 포함하는 테이블의 배열을 갖는 클래스 구현하기- template 버젼 완성하기
+* 상속 관계 : Person <- Employee
+* 상속 관계 : Student <- Workstudent
 */
 #include <iostream>
 #include <string>
@@ -7,106 +9,100 @@
 using namespace std;
 class Employee;
 class Person {
+protected:
 	string pid;
 	string pname;
 public:
+	Person() {}
 	Person(string pid, string pname) : pid(pid), pname(pname) { }
-	string GetName() { return pname; }
-	string GetId() { return pid; }
-	void SetName(char* str) { pname = str; }
 	virtual ~Person() {}
-
+	string GetName() { return pname; }
+	void SetName(char* str) { pname = str; }
+	virtual void Print() = 0;
+	virtual bool Equals(Person* p);
 };
+
+bool Person::Equals(Person* p) {
+	return pid == p->pid && pname == p->pname;
+}
 
 class Employee : public Person {
 private:
 	string eno;
 	string role;
 public:
+	Employee() : Person() {}
 	Employee(string pid, string pname, string eno, string role) : Person(pid, pname), eno(eno), role(role) { }
-
-
+	virtual ~Employee() {}
+	virtual void Print();
 	virtual bool Equals(Employee* p);
-	void Print() {
-		cout << " eno: " << eno << " role: " << role << " pid: " << GetId() << " pname: " << GetName() << endl;
-	}
 };
+
 bool Employee::Equals(Employee* p) {
-	return (eno == p->eno && role == p->role && this->GetName() == p->GetName() && this->GetId() == p->GetId());
+	return pid == p->pid && pname == p->pname && eno == p->eno && role == p->role;
 }
 
-
+void Employee::Print() {
+	cout << pid << ", " << pname << ", " << eno << ", " << role;
+}
 
 class Student {
-private:
+protected:
 	string sid;
 	string major;
 public:
-	virtual void Print();
-	Student(string sid, string major) : sid(sid), major(major) {}
-	string getSid() { return sid; }
-	string getMajor() { return major; }
+	Student() {}
+	Student(string sid, string major) : sid(sid), major(major) { }
+	virtual ~Student() {};
+	void Print();
 };
 
 void Student::Print() {
-	cout << " sid: " << sid << " major: " << major;
-
+	cout << sid << ", " << major << ", ";
 }
+
 class WorkStudent : public Student {
 private:
 	string job;
 public:
-	WorkStudent(string sid, string major, string job) :Student(sid, major), job(job) {}
+	WorkStudent() : Student() {}
+	WorkStudent(string sid, string major, string job) : Student(sid, major), job(job) { }
+	virtual ~WorkStudent() {}
 	void Print();
+	virtual bool Equals(WorkStudent* ws);
 	string GetName() { return job; }
 	void SetName(char* str) { job = str; }
-	virtual bool Equals(WorkStudent* p);
 };
-
-bool WorkStudent::Equals(WorkStudent* p) {
-	return (getSid() == p->getSid() && getMajor() == p->getMajor() && job == p->job);
-}
 
 void WorkStudent::Print() {
 	Student::Print();
-	cout << " job: " << job << endl;
+	cout << job;
+}
+
+bool WorkStudent::Equals(WorkStudent* ws) {
+	return sid == ws->sid && major == ws->major && job == ws->job;
 }
 
 
 template <class T>
-class Bag {//Bag는 템플릿 버전
+// function 들도 다 template 버전으로 만들어야 한다.
+class Bag {
 public:
 	virtual int Add(T*); // 정수 하나를 bag에 삽입
 	virtual int Delete(char*); //bag에서 정수 하나를 삭제
 	virtual T* Search(char*);
-	bool IsFull() {
-		if (topBag == bagMaxSize) {
-			Full();
-			return true;
-		}
-		return false;
-	}
+	bool IsFull();
 	// bag이 포화상태이면 true, 그렇지 않으면 false를 반환
-	bool IsEmpty() {
-		if (topBag == 0) {
-			Empty();
-			return true;
-		}
-		return false;
-	}
+	bool IsEmpty();
 	// bag이 공백 상태이면 true, 그렇지 않으면 false를 반환
 	int Top();
 	virtual void Print();
 
 protected:
 	Bag(int bagSize);
-	void Full() {
-		cout << "꽉찼습니다." << endl;
-		// bag이 포화상태일 때의 조치
-	}
-	void Empty() {
-		cout << "비어있습니다" << endl; // bag이 공백 상태일 때의 조치
-	}
+	void Full(); // bag이 포화상태일 때의 조치
+	void Empty(); // bag이 공백 상태일 때의 조치
+
 	T** arr; //> 생성자에서 arr = new T*[Size]; > arr[i] = new T();
 	int bagMaxSize; // 배열의 크기
 	int topBag; // 배열에서 원소가 들어 있는 가장 높은 위치
@@ -124,9 +120,6 @@ template <class T>
 Bag<T>::Bag(int bagSize) : bagMaxSize(bagSize) {
 	topBag = 0;
 	arr = new T * [bagSize];
-	//for (int i = 0; i < bagSize; i++) {
-	//	arr[i] = new T();
-	//}
 }
 
 template <class T>
@@ -134,30 +127,66 @@ void Bag<T>::Print() {
 	T* p;
 	for (int i = 0; i < topBag; i++) {
 		p = arr[i];
-		cout << "Bag::Print 실행gka" << endl;
 		p->Print();
+		cout << " | ";
 	}
 }
 
 template <class T>
-int Bag<T>::Add(T* p) {
-	if (topBag < bagMaxSize) {
-		arr[topBag] = p;
-		topBag++;
-		//cout << "Bag에 추가되었습니다. topBag은 "<< topBag<<"입니다." << endl;
+bool Bag<T>::IsFull() {
+	if (topBag == bagMaxSize) {
+		return true;
 	}
-	return 1;
+	else {
+		return false;
+	}
+}
+
+template <class T>
+bool Bag<T>::IsEmpty() {
+	if (topBag == 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+template <class T>
+void Bag<T>::Full() {
+	cout << "Data Structure is FULL!!!" << endl;
+}
+
+template <class T>
+void Bag<T>::Empty() {
+	cout << "Data Structure is EMPTY!!!" << endl;
+}
+
+
+template <class T>
+int Bag<T>::Add(T* x) {
+	if (IsFull()) {
+		Full();
+		return -1;
+	}
+
+	if (topBag < bagMaxSize) {
+		arr[topBag++] = x;
+		return 1;
+	}
+
+	return -1;
 }
 
 template <class T>
 int Bag<T>::Delete(char* str) {
 	for (int i = 0; i < topBag; i++) {
 		if (arr[i]->GetName() == str) {
-			arr[i]->SetName(const_cast<char*>("*****"));
+			arr[i]->SetName("*******");
 			return 1;
 		}
 	}
-	return -1;
+	return 0;
 }
 
 template <class T>
@@ -168,7 +197,7 @@ int Bag<T>::Top() {
 template <class T>
 T* Bag<T>::Search(char* str) {
 	for (int i = 0; i < topBag; i++) {
-		if (arr[i]->GetName() == str) {
+		if (strcmp(arr[i]->GetName().c_str(), str) == 0) {
 			return arr[i];
 		}
 	}
@@ -176,9 +205,9 @@ T* Bag<T>::Search(char* str) {
 }
 
 template <class T>
-class Set : public Bag <T> {
+class Set : public Bag<T> {
 public:
-	Set(int setSize) :Bag <T>(setSize) {}
+	Set(int setSize) :Bag<T>(setSize) {}
 	int Add(T*);
 	int Delete(char*);
 	void Print();
@@ -186,30 +215,21 @@ public:
 };
 
 template <class T>
-T* Set<T>::Search(char* str) {
-	return Bag<T>::Search(str);
-}
-
-template <class T>
-void Set<T>::Print() {
-	Bag<T>::Print();
-}
-
-template <class T>
 int Set<T>::Add(T* p) {
-	if (!Set<T>::IsFull()) {
-
+	if (IsFull()) {
+		Full();
+		return -1;
+	}
+	else {
 		for (int i = 0; i < topBag; i++) {
-
-
-			if ((*(arr[i])).Equals(p)) {//중복체크
-				cout << "중복이 있습니다" << endl;
-				return 1;
+			if ((*(arr[i])).Equals(p)) {
+				cout << "중복된 값입니다." << endl;
+				return -1;
 			}
 		}
 		Bag<T>::Add(p);
+		return 0;
 	}
-	return 1;
 }
 
 template <class T>
@@ -218,11 +238,30 @@ int Set<T>::Delete(char* str) {
 }
 
 template <class T>
-class RecordSet : public Set <T> {
+void Set<T>::Print() {
+	for (int i = 0; i < topBag; i++) {
+		arr[i]->Print();
+		if (i < topBag - 1) {
+			cout << " | ";
+		}
+	}
+	if (topBag > 0 && topBag % 5 != 0) {
+		cout << " | ";
+	}
+}
+
+template <class T>
+T* Set<T>::Search(char* str) {
+	return Bag<T>::Search(str);
+}
+
+
+template <class T>
+class RecordSet : public Set<T> {
 	int setMaxSize;//5개의 레코드 수
 	int topRecordSet;//해당 레코드세트에서 입력할 위치
 public:
-	RecordSet(int maxSize) :Set(maxSize), setMaxSize(maxSize) {
+	RecordSet(int maxSize) :Set<T>(maxSize), setMaxSize(maxSize) {
 		topRecordSet = 0;
 	}
 	T* Search(char*);
@@ -236,33 +275,57 @@ T* RecordSet<T>::Search(char* str) {
 }
 
 template <class T>
-int RecordSet<T>::Add(T* p) {
-	return Set<T>::Add(p);
+void RecordSet<T>::Print() {
+	Set<T>::Print();
+	if (topRecordSet > 0 && topRecordSet % 5 != 0) {
+		cout << " | ";
+	}
 }
 
 template <class T>
-void RecordSet<T>::Print() {
-	Set<T>::Print();
+int RecordSet<T>::Add(T* p) {
+	if (topRecordSet < setMaxSize) {
+		if (Set<T>::Add(p) != -1) {
+			topRecordSet++;
+			if (topRecordSet == setMaxSize) {
+				cout << "가득 찼습니다." << endl;
+				return 1; // 행이 증가함을 알리기 위해 1을 반환
+			}
+			return 0;
+		}
+	}
+
+	if (topRecordSet + 1 < setMaxSize) {
+		topRecordSet++;
+		Set<T>::Add(p);
+		return 0;
+	}
+
+	return -1; // 모든 열이 가득 찬 경우
 }
 
+
+
+// RecordTable은 Person이나 Student 의 상속을 받지 않는다. 
+// -> 이것을 template로 만들지 않으면 RecordSet이 template로 되어있기 때문에 작동하지 않는다. 다른 객체들을 넣을 때 문제가 발생한다. 그렇기 때문에 template로 만들어야 한다.
 template <class T>
 class RecordTable {
 	int tableMaxSize;
 	int topRecordTable;
+	// RecordSet** data;
+	//------------------------------------ 교수님 코드 ------------------------------------//
 	RecordSet<T>** data;
+	//------------------------------------ 교수님 코드 ------------------------------------//
 	int capacity;
-	int record;
 public:
 	RecordTable(int numberSet, int numberRecords) :tableMaxSize(numberSet), capacity(numberRecords) {
-		record = 0;
 		topRecordTable = 0;
-		data = new RecordSet<T>*[numberSet];//10개의 set
+		data = new RecordSet<T> *[numberSet];//10개의 set
 		for (int i = 0; i < numberSet; i++) {
 			data[i] = new RecordSet<T>(numberRecords);//각 set는 5개 records
 		}
-
-
 	}
+
 	int Add(T*); // 정수 하나를 bag에 삽입
 	int Delete(char*);
 	T* Search(char*);
@@ -270,64 +333,55 @@ public:
 };
 
 template <class T>
-int RecordTable<T>::Delete(char* str) {
-	for (int i = 0; i < topRecordTable; i++) {
-		//Employee* p = data[i]->Search(d);
-		//if (p != nullptr) {
-		//    cout << "없는 이름입니다."<<endl;
-		//    return -1;
-		//}
-		if (data[i]->Delete(str) == 1) {
-			record++;
+int RecordTable<T>::Add(T* p) {
+	int result = data[topRecordTable]->Add(p); // RecordSet의 Add() 메소드 호출 결과 저장
+	if (result == 1) { // 행이 증가했을 경우
+		topRecordTable++; // topRecordTable 변수를 증가시킴
+		if (topRecordTable == tableMaxSize) {
+			cout << "테이블이 가득 찼습니다." << endl;
+			return 1; // 테이블이 가득 찼음을 알리기 위해 1을 반환
 		}
-
-
 	}
-	return record;
+	return 0;
+}
 
+template <class T>
+int RecordTable<T>::Delete(char* str) {
+	int deletedCount = 0;
+
+	for (int i = 0; i < tableMaxSize; i++) {
+		if (data[i]->Delete(str) == 1) {
+			deletedCount++;
+		}
+	}
+	return deletedCount;
 }//bag에서 정수 하나를 삭제
 
 template <class T>
-int RecordTable<T>::Add(T* p) {
-	if (!data[topRecordTable]->IsFull()) {
-		data[topRecordTable]->Add(p);
-
-		return 1;
+T* RecordTable<T>::Search(char* str) {
+	for (int i = 0; i < tableMaxSize; i++) {
+		T* result = data[i]->Search(str);
+		if (result != nullptr) {
+			return result; // 레코드를 성공적으로 찾은 경우
+		}
 	}
-	else {
-		topRecordTable++;
-		//cout << "topRecordTable은" << topRecordTable << "입니다." << endl;
-		data[topRecordTable]->Add(p);
-		return 1;
-	}
-
+	return nullptr; // 찾을 레코드가 없는 경우
 }
 
 template <class T>
 void RecordTable<T>::Print() {
-	for (int i = 0; i < topRecordTable + 1; i++)
-	{
-		data[i]->Print();
-	}
-	cout << endl;
-}
-
-template <class T>
-T* RecordTable<T>::Search(char* str) {
 	for (int i = 0; i < topRecordTable; i++) {
-		T* p = data[i]->Search(str);
-		if (p != nullptr) {
-			return p;
+		data[i]->Print();
+		if (i < topRecordTable) {
+			cout << " | " << endl;
 		}
-
 	}
-	return nullptr;
 }
 
 int main() {
-	Person* members[30];//Employee 선언으로 변경하는 문제 해결 필요 
+	Person* members[30];
 	WorkStudent* workers[20];
-	RecordTable<Employee> etable(10, 5);//10개의 record sets, 각 set은 5개의 records
+	RecordTable<Employee> etable(10, 5);
 	RecordTable<WorkStudent> wtable(10, 5);
 	int select;
 	Employee* e; WorkStudent* ws;
@@ -348,7 +402,6 @@ int main() {
 			members[7] = new Employee("p8", "na", "E8", "Sales");
 			members[8] = new Employee("p9", "you", "E9", "Account");
 			members[9] = new Employee("p10", "song", "E10", "Production");
-
 			workers[0] = new WorkStudent("s011", "coding", "hong");
 			workers[1] = new WorkStudent("s012", "coding", "ong");
 			workers[2] = new WorkStudent("s013", "coding", "dong");
@@ -392,22 +445,22 @@ int main() {
 		case 3:
 			// table에서 객체 찾기
 
-			e = etable.Search(const_cast<char*>("kim"));
+			e = etable.Search("kim");
 			if (e == nullptr) cout << "kim이 존재하지 않는다" << endl;
 			else
 				e->Print();
-
-			ws = wtable.Search(const_cast<char*>("kim"));
+			cout << endl;
+			ws = wtable.Search("kim");
 			if (ws == nullptr) cout << "kim이 존재하지 않는다" << endl;
 			else
 				ws->Print();
 			break;
 		case 4:
 			//table에서 객체 삭제하기
-			result = etable.Delete(const_cast<char*>("hong"));
+			result = etable.Delete("hong");
 			if (result > 0)
 				cout << "삭제된 records 숫자" << result << endl;
-			result = wtable.Delete(const_cast<char*>("hong"));
+			result = wtable.Delete("hong");
 			if (result > 0)
 				cout << "삭제된 records 숫자" << result << endl;
 			break;
